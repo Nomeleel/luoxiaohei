43e09bcb4e319a14a4a96489ef112683360117cd
Nomeleel <choonlay@outlook.com>
2022-11-14 23:48:30 +0800

    App init on splash page.

--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../app/app_provider.dart';
 import '../../widgets/common/luo_scaffold.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends ConsumerStatefulWidget {
   const SplashPage({super.key});
+  @override
+  ConsumerState<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends ConsumerState<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(appProvider).init();
+  }
 
   @override
   Widget build(BuildContext context) {
